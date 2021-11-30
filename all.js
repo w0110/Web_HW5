700160e5efabf76a493ed4886557ecce6890669c
0110w <0110winnie@gmail.com>
2021-12-01 01:00:44 +0800

    add 11-30

---
+++ all.js
@@ -0,0 +1,13 @@
+// document.getElementsByClassName("button1").addEventListener("mousedown", off);
+// document.getElementsByClassName("button1").addEventListener("mouseup", on);
+// function on() {
+//     document.getElementsByClassName("button1").src = "https://c.tenor.com/hsLX6OyNm9gAAAAC/rex-toy-story.gif";
+// }
+// function off() {
+//     document.getElementsByClassName("button1").src = "https://media1.giphy.com/media/9zJuxb0kP8pzO/giphy.gif";
+// }
+
+
+function triggerAlert() {
+    alert('搜尋');
+}
